f309add52c0bc5e7730f3ed7dff0d111fc0943df
Daniel Beckwith <djbsnx@gmail.com>
2015-10-24 13:44:57 -0400

    Added icons and table styling

--- webui/app.js
+++ webui/app.js
@@ -4,6 +4,11 @@ app.controller('ServerDataController', function ($scope) {
   $.getJSON(window.location.protocol + '//' + window.location.hostname + ':' + 8080 + '/all_data')
     .done(function (data) {
       console.log(data);
+
+      _.forEach(data.users, function (user) {
+        user.lastLocation = _.find(data.locations, function (l) { return l._id === user.lastLocation; });
+      });
+      
       _.merge($scope, data);
       $scope.$apply();
     })
