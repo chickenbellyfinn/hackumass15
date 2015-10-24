d95b0757410b0ad1e8e2f934c89e4efae3881cb4
Aditya Nivarthi <adityanivarthi19@gmail.com>
2015-10-24 07:14:21 -0400

    Added mongodb setup script.

---
+++ apiserver/mongodbsetup.js
@@ -0,0 +1,21 @@
+// Get MongoDB client
+var dbClient = require('mongodb').MongoClient;
+var db;
+
+// Connect to MongoDB
+function getConnection(callback) {
+	if (db) {
+		callback(db);
+	}
+	else {
+		dbClient.connect("mongodb://localhost:27017/halloweenapp", function(err, dbconn) {
+		  if (!err) {
+		    console.log("Connection made");
+		    db = dbconn;
+		  	callback(db);
+		  }
+		});
+	}
+}
+
+module.exports = getConnection;
