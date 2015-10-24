4a2658617d44ae8609eae00aa4afcbc41f756a52
Daniel Beckwith <djbsnx@gmail.com>
2015-10-24 12:49:47 -0400

    Added JS support for all server data.

--- webui/app.js
+++ webui/app.js
@@ -3,7 +3,8 @@ var app = angular.module('halloweenApp', []);
 app.controller('ServerDataController', function ($scope) {
   $.getJSON(window.location.protocol + '//' + window.location.hostname + ':' + 8080 + '/all_data')
     .done(function (data) {
-      $scope.candies = data;
+      console.log(data);
+      _.merge($scope, data);
       $scope.$apply();
     })
     .fail(function () {
