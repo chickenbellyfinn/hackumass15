5ce68b59d6b31eb8c35a2fdc0f4378a125a35517
Daniel Beckwith <djbsnx@gmail.com>
2015-10-24 08:58:25 -0400

    Added Angular view update

--- webui/app.js
+++ webui/app.js
@@ -4,6 +4,7 @@ app.controller('ServerDataController', function ($scope) {
   $.getJSON(window.location.protocol + '//' + window.location.hostname + ':' + 8080 + '/all_data')
     .done(function (data) {
       $scope.candies = data;
+      $scope.$apply();
     })
     .fail(function () {
       console.log('Error getting server data');
