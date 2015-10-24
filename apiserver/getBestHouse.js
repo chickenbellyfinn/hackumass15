b0c1c6333031f725161eb098845c570c7c9c35aa
Daniel Beckwith <djbsnx@gmail.com>
2015-10-24 17:39:56 -0400

    Fixed getBestHouse

--- apiserver/getBestHouse.js
+++ apiserver/getBestHouse.js
@@ -1,6 +1,7 @@
 var dbClient = require('./mongodbsetup');
 var getDirections = require('./Directions');
 var _ = require('lodash');
+var ObjectId = require('mongodb').ObjectID;
 
 // req: JSON object containing house request parameters
 // returns: string containing address of the next best house
@@ -8,22 +9,38 @@ function getBestHouse(req, callback) {
 	var user = req.user;
 	var keywords = req.keywords;
 
+	if (!keywords.length) {
+		getRandomHouse(function (rand) {
+			callback(rand.addr);
+		});
+	}
+
 	dbClient(function(db) {
-		var regexp = RegExp(keywords.join('|'));
+		var regexp = RegExp(keywords.join('|'), 'i');
 		db.collection("candies").find({'name': {$regex: regexp}}).toArray(function(err, candiesRes) {
+			if (err || !candiesRes || !candiesRes.length) {
+				getRandomHouse(function (rand) {
+					callback(rand.addr);
+				});
+			}
 			db.collection("candyCounts").find({'candy': {$in: _.pluck(candiesRes, "_id")}}).toArray(function (err, candyCountRes) {
+				if (err || !candyCountRes || !candyCountRes.length) {
+					getRandomHouse(function (rand) {
+						callback(rand.addr);
+					});
+				}
 				var countList = _.countBy(candyCountRes, 'loc');
 				var bestCount = _.max(countList);
-				var bestLocation = _.find(countList, function(count) {
+				var bestLocation = ObjectId(_.findKey(countList, function(count) {
 					return count === bestCount;
-				});
+				}));
 
 				db.collection("locations").findOne({'_id': bestLocation}, function(err, entity) {
 					var add2 = entity.addr;
 					var add1;
-					db.collection("users").find({'name': user}, function(results) {
+					db.collection("users").findOne({'name': user}, function(err, results) {
 						var id = results.lastLocation;
-						db.collection("locations").find({'_id': id}, function(loc) {
+						db.collection("locations").findOne({'_id': id}, function(err, loc) {
 							add1 = loc.addr;
 							getDirections(add1, add2, callback);
 						});
@@ -31,6 +48,12 @@ function getBestHouse(req, callback) {
 				});
 			});
 		});
+	});
+}
+
+function getRandomHouse(callback) {
+	db.collection('locations').find({}).toArray(function (err, locations) {
+		callback(locations[Math.floor(Math.random() * locations.length)]);
 	});
 }
 
