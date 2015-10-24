e5fb94e982e13cf2a21b94060a84f9a37409db2a
Aditya Nivarthi <adityanivarthi19@gmail.com>
2015-10-24 12:42:00 -0400

    Added getAllData to give all results of collections. Added callback for getBestHouse.

--- apiserver/getAllData.js
+++ apiserver/getAllData.js
@@ -3,17 +3,36 @@ var dbClient = require('./mongodbsetup');
 // returns all the location data in the database as a JSON object
 function getAllData(callback) {
 	dbClient(function (db) {
-    db.collection('candies', function (err, candies) {
-      if (err) console.log('Error: ' + err);
-      else {
-        candies.find().toArray(function (err, results) {
-          if (err) console.log('Error: ' + err);
-          else {
-            callback(results);
-          }
-        });
+    var candies = db.collection('candies');
+    var users = db.collection('candies');
+    var locations = db.collection('candies');
+    var candyCounts = db.collection('candies');
+    
+    var names = ["candies", "users", "locations", "candyCounts"];
+    var collections = [];
+
+    for (var i = 0; i < names.length; i++) {
+      collections.push(db.collection(names[i]));
+    };
+
+    var allresults;
+
+    function recurser(i, results) {
+      if (i >= 0) {
+        allresults[names[i]] = results;
       }
-    });
+
+      if (i >= names.length) {
+        callback(allresults);
+        return;
+      }
+
+      collections[i + 1].find().toArray(function(err, results) {
+        recurser(i + 1, results);
+      });
+    }
+
+    recurser(-1, null);
 	});
 }
 
